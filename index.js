adb235c5541e0449e984aa684472dbbf5df4d260
wara <144021938+waraumbach@users.noreply.github.com>
2024-03-20 13:36:34 +0100

    updated post

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express, { request, response } from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const myUsers = [
   {
